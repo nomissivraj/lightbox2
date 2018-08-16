c126ded2a20c9175e7aff77c62f7bfa112ebe002
Simon Jarvis <simonpeterjarvis@gmail.com>
2018-08-16 10:37:52 +0100

    Starting main functions/structure

--- lightbox2/scripts/lightbox_project.js
+++ lightbox2/scripts/lightbox_project.js
@@ -1,4 +1,4 @@
-/* PSEUDO CODE
+/* PSEUDO CODE & 
 *   Individual modules that launch 
 *   Work out how i'm going to detect mode - try not to go overly complicated with this. Data attributes will work?
 */
@@ -6,6 +6,34 @@
 //variables
 
 //Main functions
+
+function modeCheck() {
+    //switch case
+}
+
+function lightbox() {
+    
+}
+
+function imageBox() {
+    lightbox();
+    //image specific stuff here
+}
+
+function galleryBox() {
+    lightbox();
+    //gallery specific stuff here
+}
+
+function modalBox() {
+    lightbox();
+    //modal specific stuff here
+}
+
+function iframe() {
+    lightbox();
+    //iframe specific stuff here
+}
 
 //other handler functions
 
