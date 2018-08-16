2dcfc4be4f65d5ea76ed0a3a13f863d97b22dc89
nomissivraj <simonpeterjarvis@gmail.com>
2018-08-16 10:06:10 +0100

    Starting project
    Code comments/pseudo code outlining direction of project

---
+++ lightbox2/scripts/lightbox_project.js
@@ -0,0 +1,16 @@
+/* PSEUDO CODE
+*   Individual modules that launch 
+*   Work out how i'm going to detect mode - try not to go overly complicated with this. Data attributes will work?
+*/
+
+//variables
+
+//Main functions
+
+//other handler functions
+
+//behaviour
+
+//tab restrictions - accessibility
+
+
